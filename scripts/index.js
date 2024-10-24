5d7d192bb189a9c4623131126da4e7ed17c210ad
Anton Emelyanov <emelyanov.a.v.mail@gmail.com>
2024-10-24 19:17:58 +0500

    добавлены функции создания и удаления

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+
+
 // @todo: Функция удаления карточки
+function deleteCard (event) {
+    const cardDelete = event.target.parentNode;
+    cardDelete.remove();
+}
+
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+   cardElement.querySelector('.card__title').textContent = item.name;
+   cardElement.querySelector('.card__image').src = item.link;
+   cardElement.querySelector('.card__delete-button').addEventListener('click', function (event) {
+       deleteCard(event);
+   });
+
+   placesList.append(cardElement);
+
+});
+
+
